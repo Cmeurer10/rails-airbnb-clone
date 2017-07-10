af25c1618042ef09f7d2c2e021ab14aa8f40ca20
Cole Meurer <cole.meurer@gmail.com>
2017-07-10 16:16:05 +0900

    validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :purchases
-  has_many :books_purchased, through: :purchases, source: :book#, foreign_key: "book_id"
+  has_many :books_purchased, through: :purchases, source: :book
+  validates :email, presence: true
+  validates_format_of :email,:with => Devise::email_regexp
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :university, presence: true
 end
