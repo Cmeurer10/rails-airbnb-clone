aeddb81bbef826ad47bd4862acad2ed44a0d32b6
Cole Meurer <cole.meurer@gmail.com>
2017-07-10 16:25:35 +0900

    continued validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,11 +4,12 @@ class Book < ApplicationRecord
   has_one :buyer, through: :purchase, source: :user
   validates :title, presence: true
   validates :edition, presence: true
-  validates :condition, presence: true
+  validates :condition, presence: true, includsion: { in: ['Good', 'Like New', 'Poor', 'Fair'] }
   validates :price, presence: true, numericality: true
   validates :subject, presence: true
   validates :isbn, presence: true
   validates :publisher, presence: true
+
   def sold?
     return !buyer.nil?
   end
