83f5f762431586e21e9eeee929af9cc827ce3e61
Cole Meurer <cole.meurer@gmail.com>
2017-07-12 16:19:57 +0900

    search bar routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   resources :books, only: [:index]
-  get '/dashboard', to: 'pages#dashboard' do
-    resources :books, except: [:show, :index]
-  end
+  get '/dashboard', to: 'pages#dashboard'
   get '/dashboard/new', to: 'books#new'
   post '/dashboard', to: 'books#create'
-  get '/books/:title', to: 'books#show'
+  get '/books/list/:title', to: 'books#show'
+  post '/books/list/:title', to: 'books#show'
   get '/dashboard/update/:id', to: 'books#edit'
   patch '/dashboard/:id', to: 'books#update'
   delete '/dashboard/:id', to: 'books#destroy'
