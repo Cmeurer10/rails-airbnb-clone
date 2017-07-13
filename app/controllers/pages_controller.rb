5ab85f1d7bcc959f776a83df09f4ba84e33291d5
Owen Klumb <owen.klumb@gmail.com>
2017-07-13 15:47:52 +0900

    added image tags for books

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
+    @purchases = @user.purchases
     purchases = Purchase.all.where(user: current_user, finalized: true)
     @books_purchased = purchases.map { |pur| pur.book }
     @delete_message = "Are you sure? This will remove the book from your dashboard as well."
