dac4d63ccea842a4ff27e319c0cad1223c97066e
Cole Meurer <cole.meurer@gmail.com>
2017-07-14 13:46:28 +0900

    fixed seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :nullify
   has_many :purchases, dependent: :nullify
   has_many :books_purchased, through: :purchases, source: :book, dependent: :nullify
-  #validates :email, presence: true
-  validates_format_of :email,:with => Devise::email_regexp
+  # validates :email, presence: false
+  # validates_format_of :email,:with => Devise::email_regexp
   validates :password, presence: true, length: { in: 6..20 }
   validates :first_name, presence: true
   validates :last_name, presence: true
